5c91f34ac77970be59305a49f3ab36d2a4c664d8
aikongmeng <aikongmeng@gmail.com>
2021-08-20 18:16:58 +0800

    setOnClickListener

--- app/src/main/java/com/example/paging3sample/RepoAdapter.kt
+++ app/src/main/java/com/example/paging3sample/RepoAdapter.kt
@@ -3,6 +3,7 @@ package com.example.paging3sample
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.TextView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
@@ -10,43 +11,68 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.paging3sample.model.Repo
 
 class RepoAdapter(val itemClick: (html_url: String) -> Unit) :
-    PagingDataAdapter<Repo, RepoAdapter.ViewHolder>(COMPARATOR) {
-
+    PagingDataAdapter<Repo, RepoAdapter.ViewHolder>(POST_COMPARATOR) {
     companion object {
-        private val COMPARATOR = object : DiffUtil.ItemCallback<Repo>() {
-            override fun areItemsTheSame(oldItem: Repo, newItem: Repo): Boolean {
-                return oldItem.id == newItem.id
-            }
+        val POST_COMPARATOR = object : DiffUtil.ItemCallback<Repo>() {
+            override fun areContentsTheSame(oldItem: Repo, newItem: Repo): Boolean =
+                oldItem == newItem
 
-            override fun areContentsTheSame(oldItem: Repo, newItem: Repo): Boolean {
-                return oldItem == newItem
-            }
+            override fun areItemsTheSame(oldItem: Repo, newItem: Repo): Boolean =
+                oldItem.name == newItem.name
         }
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name: TextView = itemView.findViewById(R.id.name_text)
-        val description: TextView = itemView.findViewById(R.id.description_text)
-        val starCount: TextView = itemView.findViewById(R.id.star_count_text)
-        val language: TextView = itemView.findViewById(R.id.language_text)
+    class ViewHolder(itemView: View, var itemClick: (html_url: String) -> Unit) :
+        RecyclerView.ViewHolder(itemView) {
+
+        private val name: TextView = itemView.findViewById(R.id.name_text)
+        private val description: TextView = itemView.findViewById(R.id.description_text)
+        private val starCount: TextView = itemView.findViewById(R.id.star_count_text)
+        private val language: TextView = itemView.findViewById(R.id.language_text)
+        private var repo: Repo? = null
+
+        init {
+            itemView.setOnClickListener {
+                repo?.html_url?.let { it1 -> itemClick(it1) }
+            }
+        }
+
+        fun bind(repo: Repo?) {
+            this.repo = repo
+            if (repo != null) {
+                name.text = repo.name
+                description.text = repo.description
+                starCount.text = repo.starCount.toString()
+                language.text = repo.language
+            }
+        }
+
+        fun update(repo: Repo?) {
+            this.repo = repo
+            starCount.text = repo?.starCount.toString()
+        }
+
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.repo_item, parent, false)
-        val viewHolder = ViewHolder(view)
-        view.setOnClickListener {
-            getItem(viewHolder.layoutPosition)?.html_url?.let { it1 -> itemClick(it1) }
+
+
+        return ViewHolder(view) {
+            itemClick(it)
         }
-        return viewHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val repo = getItem(position)
-        if (repo != null) {
-            holder.name.text = repo.name
-            holder.description.text = repo.description
-            holder.starCount.text = repo.starCount.toString()
-            holder.language.text = repo.language
+        holder.bind(getItem(position))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+        if (payloads.isNotEmpty()) {
+            holder.update(getItem(position))
+        } else {
+            onBindViewHolder(holder, position)
         }
     }
 
